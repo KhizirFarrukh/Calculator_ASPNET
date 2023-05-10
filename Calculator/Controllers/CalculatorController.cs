beae12a3c63ede7c3f0ef6c3cfb110e3906a3530
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-05-10 11:20:32 +0500

    Calculator web app ready

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public ActionResult Index()
+        {
+            var viewModel = new CalculatorViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculatorViewModel viewModel)
+        {
+            switch (viewModel.Operation)
+            {
+                case "+":
+                    viewModel.Result = viewModel.Number1 + viewModel.Number2;
+                    break;
+                case "-":
+                    viewModel.Result = viewModel.Number1 - viewModel.Number2;
+                    break;
+                case "*":
+                    viewModel.Result = viewModel.Number1 * viewModel.Number2;
+                    break;
+                case "/":
+                    viewModel.Result = viewModel.Number1 / viewModel.Number2;
+                    break;
+            }
+
+            return View(viewModel);
+        }
+    }
+}
+
---
+++ Calculator/Models/CalculatorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Calculator.Models
+{
+    public class CalculatorViewModel
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public string Operation { get; set; }
+        public List<string> OperationList { get; set; } = new List<string>() { "+", "-", "*", "/" };
+        public int? Result { get; set; }
+    }
+}
